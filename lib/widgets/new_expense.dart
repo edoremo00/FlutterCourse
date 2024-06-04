59a0e7ea08b617a6f29ec69ada22e041c64539cf
Edoardo Remondini <edoardoremondini@outlook.it>
2024-06-04 19:18:09 +0200

    added spacing between category chips and improved design, added selection logic for category chips, added validation and error message widget

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,6 +1,9 @@
 import 'package:expensetracker/models/category.dart';
 import 'package:expensetracker/models/expense.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 
 class NewExpense extends StatefulWidget {
@@ -17,6 +20,9 @@ class _NewExpenseState extends State<NewExpense> {
 
   DateTime? _pickedDate;
   String currencySymbol="";
+  
+
+  Category? _selectedCategory;
 
 
   @override
@@ -44,16 +50,83 @@ class _NewExpenseState extends State<NewExpense> {
 
   void _openDatepicker() async{
     final now=DateTime.now();
-    final firstdate=DateTime(now.year-1,now.month,now.day);
+    final firstdate=DateTime(now.year-1,DateTime.january);
     final pickedDate=await showDatePicker(
       context: context,
       firstDate: firstdate,
       lastDate: now
     );
-    _pickedDate=pickedDate;
     setState(() {
-      
+      _pickedDate=pickedDate;
     });
+  }
+
+  void _submitExpenseData(){
+    final enteredAmount = double.tryParse(_amountController.text);
+    final isAmountinvalid = enteredAmount == null || enteredAmount <= 0;
+    final isCategoryinvalid = _selectedCategory == null;
+    final isDateinvalid = _pickedDate == null;
+    if (_titleController.text.trim().isEmpty ||
+        isAmountinvalid ||
+        isCategoryinvalid ||
+        isDateinvalid) {
+      //error
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          contentPadding: EdgeInsets.zero,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                height: 150,
+                padding: EdgeInsets.zero,
+                decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 229, 174, 9),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    )),
+                child: const Center(
+                  child: Icon(Icons.sentiment_dissatisfied,size: 85,),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const Text(
+                "Oh no!",
+                style: TextStyle(fontSize: 24),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                child: Text(
+                  "Please make sure a value is provided for all the fields",
+                  style: TextStyle(fontSize: 16),
+                ),
+              )
+            ],
+          ),
+          // content: const Text("Please make sure the fields have a value"),
+          actions: [
+            TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text("Got it!"),)
+          ],
+        ),
+      );
+      return;
+    }
+    //TODO add logic for adding and refresh expense list
+    Expense newexp = Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date: _pickedDate!,
+        category: _selectedCategory!);
+   
   }
 
 
@@ -71,8 +144,8 @@ class _NewExpenseState extends State<NewExpense> {
           right: 16),
       child: Column(
         mainAxisSize: MainAxisSize.min,
-        // crossAxisAlignment: CrossAxisAlignment.end,
-        //TODO fix X button alignment
+        crossAxisAlignment: CrossAxisAlignment.end,
+        //TODO test multiple chips inside a row with mainaxisalignemt to see if spacing works
         children: [
           IconButton(
             onPressed: () {
@@ -135,23 +208,33 @@ class _NewExpenseState extends State<NewExpense> {
               
             ],
           ),
-          const SizedBox(height: 16),
-          SingleChildScrollView(scrollDirection: Axis.horizontal,child: Row(
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //TODO fix spacing between Chip
+          const SizedBox(height: 32),
+          SingleChildScrollView(scrollDirection: Axis.horizontal,clipBehavior: Clip.none,child: Wrap(
+            spacing: 16,
             children: [
               ...categoryIcons.entries.map(
-                (entry) => Chip(
-                  avatar: Icon(entry.value),
-                  label: Text(entry.key.name),
-                ),
-              )
+                (entry) {
+                  return ChoiceChip(
+                  showCheckmark: false,
+                  selected: _selectedCategory==entry.key,
+                  onSelected: (_){
+                    setState(() {
+                      _selectedCategory=entry.key;
+                    });
+                  },
+                  padding: const EdgeInsets.all(6),
+                  labelPadding: const EdgeInsets.all(6),
+                  avatar: CircleAvatar(child: Icon(entry.value),),
+                  label: Text(entry.key.name.toUpperCase(),),
+                );} 
+                
+              ),
             ],
           ),),
           Row(
             children: [
               ElevatedButton(
-                onPressed: () {},
+                onPressed: _submitExpenseData,
                 child: const Text("Save"),
               ),
             ],
