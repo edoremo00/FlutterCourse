495b2b50ab8c6d503ee0b6b2ac5748fb7e8e5d04
Edoardo Remondini <edoardoremondini@outlook.it>
2024-07-30 19:31:02 +0200

    inizio implementazione filtri aggiuntivi

---
+++ lib/models/sorting.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+enum Sorting {expensename,price,date}
+
+//map
+const sortingIcons={
+  Sorting.expensename:Icons.abc,
+  Sorting.price:Icons.money,
+  Sorting.date:Icons.calendar_month
+};
