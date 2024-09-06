e02ce1b1c0cf3c3b736990ac164d656021df5f01
Edoardo Remondini <edoardoremondini@outlook.it>
2024-09-06 17:37:19 +0200

    aggiunta categoria altro in pannello laterale filtri

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
-enum Category {food,travel,leisure,work}
+enum Category {food,travel,leisure,work,other}
 
 //map
-const categoryIcons={
+ Map<Category,IconData?> categoryIcons={
   Category.food:Icons.lunch_dining,
   Category.travel:Icons.flight_takeoff,
   Category.leisure:Icons.movie,
   Category.work:Icons.work,
+  Category.other:null
 };
