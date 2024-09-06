7d602a24fe4385d675ca67ab4918b782ec9ad6c4
Edoardo Remondini <edoardoremondini@outlook.it>
2024-09-06 17:40:15 +0200

    gestione tasto indietro per refresh filtri

--- lib/widgets/searchpage.dart
+++ lib/widgets/searchpage.dart
@@ -1,11 +1,12 @@
 import 'package:expensetracker/models/expense.dart';
+import 'package:expensetracker/models/filter.dart';
 import 'package:expensetracker/widgets/expenses-list/expenses_list.dart';
 import 'package:flutter/material.dart';
 
 
 class SearchPage extends StatefulWidget{
 
-  final List<Expense> Function(String? searchText) onExpenseSearch;
+  final List<Expense> Function({Filter? filterObj, String? searchText}) onExpenseSearch;
   final void Function({Expense? expenseToedit,void Function()? handleSearchRefresh}) onModifyswipeDirection;
   final void Function(Expense exp,{ void Function()? handleSearchRefresh}) onRemoveExpense;
   final String currencySymbol;
@@ -32,68 +33,74 @@ class Searchpagestate extends State<SearchPage>{
     //   widget.onExpenseSearch(searchController.text);
     // });
     setState(() {
-      filteredExpenses=widget.onExpenseSearch(searchController.text);
+      filteredExpenses=widget.onExpenseSearch(searchText:searchController.text);
     });
   }
 
   final TextEditingController searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
-      appBar: AppBar(
-        actions:   [
-          const SizedBox(width: 50,),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  child: TextField(
-                    autofocus: true,
-                    controller: searchController,
-                    onChanged: (value){
-                      setState(() {
-                        filteredExpenses=widget.onExpenseSearch(value);
-                      });
-                    },
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "Search expenses",
-                      hintStyle: TextStyle(
-                        color: Colors.white.withOpacity(0.5)
-                      )
+    return PopScope(
+      onPopInvoked: (didPop) {
+        searchController.clear();
+        handleRefresh();
+      },
+      child: Scaffold(
+        // backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+        appBar: AppBar(
+          actions:   [
+            const SizedBox(width: 50,),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    child: TextField(
+                      autofocus: true,
+                      controller: searchController,
+                      onChanged: (value){
+                        setState(() {
+                          filteredExpenses=widget.onExpenseSearch(searchText:value);
+                        });
+                      },
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search expenses",
+                        hintStyle: TextStyle(
+                          color: Colors.white.withOpacity(0.5)
+                        )
+                      ),
+                      
                     ),
-                    
                   ),
-                ),
-                
-              ],
-            ),
-          ),
-          if (searchController.text.isNotEmpty) ...[
-            SizedBox(
-              width: 50,
-              child: IconButton(
-                onPressed: () {
-                  searchController.clear();
-                  handleRefresh();
-                },
-                icon: const Icon(Icons.close),
+                  
+                ],
               ),
             ),
-          ]
-        ],
+            if (searchController.text.isNotEmpty) ...[
+              SizedBox(
+                width: 50,
+                child: IconButton(
+                  onPressed: () {
+                    searchController.clear();
+                    handleRefresh();
+                  },
+                  icon: const Icon(Icons.close),
+                ),
+              ),
+            ]
+          ],
+        ),
+        body: filteredExpenses.isNotEmpty || searchController.text.isEmpty ?
+           Column(children: [
+             ExpensesList.filtered(filteredExpenses, widget.currencySymbol, widget.onModifyswipeDirection, widget.onRemoveExpense,handleRefresh)
+           ],)
+          
+            : const Padding(
+                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: Text("No expense found"),
+              ),
       ),
-      body: filteredExpenses.isNotEmpty || searchController.text.isEmpty ?
-         Column(children: [
-           ExpensesList.filtered(filteredExpenses, widget.currencySymbol, widget.onModifyswipeDirection, widget.onRemoveExpense,handleRefresh)
-         ],)
-        
-          : const Padding(
-              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-              child: Text("No expense found"),
-            ),
     );
   }
     
