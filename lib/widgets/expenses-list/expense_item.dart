c1d607ca5dc3c812105bc01cc8183b5240d58a63
Edoardo Remondini <edoardoremondini@outlook.it>
2024-09-06 17:41:05 +0200

    Ui card singola spesa modificata

--- lib/widgets/expenses-list/expense_item.dart
+++ lib/widgets/expenses-list/expense_item.dart
@@ -5,27 +5,51 @@ import 'package:flutter/material.dart';
 class ExpenseItem extends StatelessWidget {
   final Expense expenseitem;
   String currencySymbol;
-  ExpenseItem({super.key, required this.expenseitem,required this.currencySymbol});
+  ExpenseItem(
+      {super.key, required this.expenseitem, required this.currencySymbol});
   @override
   Widget build(BuildContext context) {
     return Card(
+      // color: expenseTypes[expenseitem.expenseType]!.shade400,
+      // child: Padding(
+      //   padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+      //   child: Column(
+      //     crossAxisAlignment: CrossAxisAlignment.start,
+      //     children: [
+      //       Text(expenseitem.title,style: Theme.of(context).textTheme.titleLarge,),
+      //       Icon(categoryIcons[expenseitem.category]),
+      //       const SizedBox(
+      //         height: 4,
+      //       ),
+      //       Row(
+      //         children: [
+      //           Text(
+      //            "$currencySymbol${expenseitem.amount.toStringAsFixed(2)}"
+      //           ),
+      //           const Spacer(),
+      //           // Icon(categoryIcons[expenseitem.category]),
+      //           const SizedBox(width: 5,),
+      //           Text(expenseitem.formattedDate)
+      //         ],
+      //       )
+      //     ],
+      //   ),
+      // ),
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(expenseitem.title,style: Theme.of(context).textTheme.titleLarge,),
-            const SizedBox(
-              height: 4,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(expenseitem.title),
+                Icon(categoryIcons[expenseitem.category]),
+              ],
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                 "$currencySymbol${expenseitem.amount.toStringAsFixed(2)}"
-                ),
-                const Spacer(),
-                Icon(categoryIcons[expenseitem.category]),
-                const SizedBox(width: 5,),
+                Text("$currencySymbol${expenseitem.amount.toStringAsFixed(2)}"),
                 Text(expenseitem.formattedDate)
               ],
             )
